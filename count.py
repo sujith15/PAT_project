b4f026e6b4e09de621b9af065dd236c966395396
sujithreddydharmareddy <dharmareddysujith15@gmail.com>
2022-04-25 19:20:00 -0500

    test folder files & changed bar plot

--- count.py
+++ count.py
@@ -11,29 +11,36 @@ import matplotlib.pyplot as plt
 
 def is_file(filepath):
     print(filepath)
-    file = open(filepath, 'r', encoding='utf-8')
     assert_count = 0
-    if ".DS_Store" not in filepath and filepath.endswith(".c"):
+    if ".DS_Store" not in filepath and (filepath.endswith(".c") or filepath.endswith(".h") or filepath.endswith(".sh")):
+        file = open(filepath, 'r', encoding='utf-8')
+        # assert_count = 0 if ".DS_Store" not in filepath and (filepath.endswith(".c") or filepath.endswith(".h") or
+        # filepath.endswith(".sh")) :
         lines = file.readlines()
 
         for line in lines:
             if re.search(r'^\s*.(assert\(.*\))', line):
                 assert_count = assert_count + 1
+    else:
+        return -1
     return assert_count
 
 
-def is_directory(dir, f):
+def is_directory(dir, subdir, f):
     list = []
     for filename in os.listdir(f):
         filepath = os.path.join(f, filename)
         print(filepath)
         if os.path.isfile(filepath):
+            print("hello")
             assert_count = is_file(filepath)
-            if assert_count != 0:
-                list1 = [filepath, dir, filename, assert_count]
+            if assert_count != -1:
+                # if assert_count != 0:
+                list1 = [filepath, dir, subdir, filename, assert_count]
                 list.append(list1)
         elif os.path.isdir(filepath):
-            list = is_directory(dir, filepath)
+
+            list = is_directory(dir, subdir, filepath)
 
     return list
 
@@ -43,42 +50,43 @@ with open('assert_count.csv', 'w', encoding='utf8', newline='') as csvfile:
         print("root is..", root)
         print("dirs is...", dirs)
         print("files is..", files)
-        header = ['Path', 'Directory', 'File Name', 'Assert count']
+        header = ['Path', 'Directory', 'Sub Directory', 'File Name', 'Assert count']
         writer = csv.writer(csvfile)
         writer.writerow(header)
 
         for dir in dirs:
             print("dir is...", dir)
-            test_path = root + "/" + dir
-            for subdir in os.listdir(test_path):
-                f = os.path.join(test_path, subdir)
-                print(f)
-                if os.path.isdir(f):
+            if dir == 'test':
+                test_path = root + "/" + dir
+                for subdir in os.listdir(test_path):
+                    f = os.path.join(test_path, subdir)
+                    print(f)
+                    if os.path.isdir(f):
 
-                    list = is_directory(dir, f)
+                        list = is_directory(dir, subdir, f)
 
-                    writer.writerows(list)
+                        writer.writerows(list)
 
-                elif os.path.isfile(f):
-                    assert_count = is_file(f)
-                    if assert_count != 0:
-                        writer.writerow([f, "", f, assert_count])
+                    elif os.path.isfile(f):
+                        if f.endswith(".c") or f.endswith(".h") or f.endswith(".sh"):
+                            assert_count = is_file(f)
+                            if assert_count != -1:
+                                writer.writerow([f, dir, subdir, subdir, assert_count])
 
         break
 
 data = pd.read_csv("assert_count.csv")
-dirs = data["Directory"]
+dirs = data["Sub Directory"]
 count = data["Assert count"]
 
 x = []
 y = []
 x = dirs
 y = count
-plt.bar(x, y)
-plt.xlabel('Folders')
-plt.ylabel('Assert count')
+plt.barh(x, y)
+plt.xlabel('Assert count')
+plt.ylabel('Folders')
 plt.title('Data')
 
 plt.savefig("assert_plot.png")
 plt.show()
-
