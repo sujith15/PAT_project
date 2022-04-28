3dc7d1bcb4bc56df7740e21e3692be8dbb068a75
sujithreddydharmareddy <dharmareddysujith15@gmail.com>
2022-04-27 19:46:25 -0500

    added pydriller code

--- pydriller_task4.py
+++ pydriller_task4.py
@@ -8,7 +8,6 @@ with open('assert_count.csv') as csv_file:
         test_files.append(row['File Name'])
 count = 0
 files = []
-names=[]
 d = {}
 mod = 0
 i = 0
@@ -17,17 +16,15 @@ for commit in Repository('https://github.com/open-mpi/ompi', filepath="test").tr
         if file.filename in test_files and file.filename not in files:
             files.append(file.filename)
             count = count + 1
-            d[file.filename] = [[], [], [0], [0]]
-            if commit.author.name not in names:
-                names.append(commit.author.name)
+            d[file.filename] = [[], [], [0],[0]]
+            if commit.author.name not in d[file.filename][0]:
                 d[file.filename][0].append(commit.author.name)
                 d[file.filename][3][0] += 1
             d[file.filename][1].append(commit.author_date.isoformat())
         elif file.filename in files:
             d[file.filename][2][0] += 1
-            if commit.author.name not in names:
+            if commit.author.name not in d[file.filename][0]:
                 d[file.filename][3][0] += 1
-                names.append(commit.author.name)
                 d[file.filename][0].append(commit.author.name)
 
         # print('The file {} is created on {} by {}.'.format(file.filename,commit.author_date,commit.author.name))
@@ -35,6 +32,14 @@ for commit in Repository('https://github.com/open-mpi/ompi', filepath="test").tr
 print(d)
 print(len(d))
 print(count)
+
+filename = 'pydriller_task4.csv'
+with open(filename, 'w') as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["FileName", "List Of Authors", "Date Created", "Number of Commits", "Number of authors"])
+    for key, value in d.items():
+        csvwriter.writerow([key, ','.join(value[0]), value[1][0], value[2][0], value[3][0]])
 
 # print(
 #     'The file {} was added by {} on {}, '
