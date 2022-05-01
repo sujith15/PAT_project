e061114d67cd55502c829fd299bae86a29ad2c37
satyashodhanagaddam <satyashodhana14@gmail.com>
2022-05-01 11:51:35 -0500

    pydriller

--- pydriller_task4.py
+++ pydriller_task4.py
@@ -28,15 +28,6 @@ for commit in Repository('https://github.com/open-mpi/ompi',filepath='test').tra
             if commit.author.name not in d[file.filename][0]:
                 d[file.filename][3][0] += 1
                 d[file.filename][0].append(commit.author.name)
-        if commit.author.name in authorCommitCount.keys():
-            authorCommitCount[commit.author.name] += 1
-        else:
-            authorCommitCount[commit.author.name] = 0
-            year = commit.author_date.isoformat()[0:4]
-        if year in yearCommitCount.keys():
-            yearCommitCount[year] += 1
-        else:
-            yearCommitCount[year] = 0
 
 filename = 'pydriller_task4.csv'
 with open(filename, 'w') as csvfile:
@@ -45,14 +36,14 @@ with open(filename, 'w') as csvfile:
     for key, value in d.items():
         csvwriter.writerow([key, ','.join(value[0]), value[1][0], value[2][0], value[3][0]])
 
-with open('author_count.csv', 'w') as csvfile:
-    csvwriterAuthors = csv.writer(csvfile)
-    csvwriterAuthors.writerow(["Author Name", "Count"])
-    for key, value in authorCommitCount.items():
-        csvwriterAuthors.writerow(([key, value]))
-
-with open('commit_count.csv', 'w') as csvfile:
-    csvwriterYears = csv.writer(csvfile)
-    csvwriterYears.writerow(["Date", "Count"])
-    for key, value in yearCommitCount.items():
-        csvwriterYears.writerow(([key, value]))
+# with open('author_count.csv', 'w') as csvfile:
+#     csvwriterAuthors = csv.writer(csvfile)
+#     csvwriterAuthors.writerow(["Author Name", "Count"])
+#     for key, value in authorCommitCount.items():
+#         csvwriterAuthors.writerow(([key, value]))
+#
+# with open('commit_count.csv', 'w') as csvfile:
+#     csvwriterYears = csv.writer(csvfile)
+#     csvwriterYears.writerow(["Date", "Count"])
+#     for key, value in yearCommitCount.items():
+#         csvwriterYears.writerow(([key, value]))
